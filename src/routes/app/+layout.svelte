09a3c572e1fdc65cf7ace8af9625ddd83bb804e8
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-06-19 08:56:48 -0400

    icons and create app page

---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,21 @@
+
+<div class="wrap">
+	<nav>
+		<a href="/app">Dashboard</a>
+	</nav>
+	<slot/>
+</div>
+
+<style lang="scss">
+	.wrap {
+		height: 100%;
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+
+
+		nav {
+			height: 100%;
+		}
+	}
+</style>
