7979f9fc8377b7253e6a97db25b9f8807d195c64
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2024-06-20 14:01:12 -0400

    remove unused onMount

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { createEventDispatcher, onMount } from 'svelte';
+	import { createEventDispatcher } from 'svelte';
 	import { fly } from 'svelte/transition';
 
 	const dispatcher = createEventDispatcher();
